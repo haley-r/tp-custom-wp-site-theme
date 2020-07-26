464bcc0e324f663fb81f6241bb2f283b69bba7ab
haley-r <haleyryan77@gmail.com>
2020-07-26 14:34:55 -0500

    front page mobile first styling

--- front-page.php
+++ front-page.php
@@ -1,45 +1,18 @@
 <?php get_header(); ?>
 
-<!-- the html content of the home page would go here -->
-
 <div class="page-background-image" style="background-image: url(<?php echo get_theme_file_uri('/images/occupy_oakland.jpg'); ?>);">
     <div class="page-background-gradient">
     </div>
 </div>
-
-<section class="main-content">
-    <h2 class="intro">
-        Tim Phillips is an experienced trial lawyer who listens to his clients.
-    </h2>
-
-    <div class="floating-block">
-        <h3>He gathers and evaluates information about:</h3>
-        <ul>
-            <li>your goals</li>
-            <li>the law</li>
-            <li>the facts</li>
-            <li>non-legal aspects of the situation</li>
-        </ul>
+<?php
+while (have_posts()) {
+    the_post(); ?>
+    <h1 class="page-title"><?php the_title(); ?></h1>
+    <div class="content-block">
+        <?php the_content(); ?>
     </div>
-    <div class="floating-block">
-        <h3>He communicates with his clients about:</h3>
-        <ul>
-            <li>what’s happening in their cases</li>
-            <li>possible solutions</li>
-            <li>the advantages, disadvantages, and odds of success for each one</li>
-            <li>disparities that marginalized people face in the legal system</li>
-        </ul>
-    </div>
-    <div class="floating-block">
-        <h3>He asks each client to:</h3>
-        <ul>
-            <li>share their insights</li>
-            <li>participate in framing the dispute</li>
-            <li>choose the best of the possible solutions</li>
-            <li>participate in implementing the option that's been agreed on</li>
-        </ul>
-    </div>
-
-</section>
+<?php
+}
+?>
 
 <?php get_footer(); ?>
