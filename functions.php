5d118d2e01e847337b1f837d83ce1923ae0383d6
haley-r <haleyryan77@gmail.com>
2020-07-27 02:13:41 -0500

    deleted unused css folders and commented out Masonry import in funcitons.php

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@
         // I'm not sure if the following line is needed, it doesn't seem to make a difference locally.
         wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&family=Red+Hat+Text:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
         wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_script('masonry-layout', '//unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', NULL, '1.0', true);    
+        // wp_enqueue_style('site-main-styles', get_stylesheet_uri());
+        // wp_enqueue_script('masonry-layout', '//unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', NULL, '1.0', true);    
         wp_enqueue_script('main-university-js', 'http://localhost:4000/bundled.js', NULL, '1.0', true);    
     }
     add_action('wp_enqueue_scripts', 'site_files');
